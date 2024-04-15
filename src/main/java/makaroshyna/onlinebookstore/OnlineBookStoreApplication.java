e6e8afac57ad0380ad70628096326e8370ca7f80
makaroshyna <katya23225@gmail.com>
2024-04-15 17:04:51 +0300

    Add CommandLineRunner bean.

--- src/main/java/makaroshyna/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/makaroshyna/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,13 +1,18 @@
 package makaroshyna.onlinebookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(OnlineBookStoreApplication app) {
+        return args -> {};
+    }
 }
