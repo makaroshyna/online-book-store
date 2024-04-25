a2a66862d9b238970e3c1928a306c7112f615f18
Katia Makarchuk <83081198+makaroshyna@users.noreply.github.com>
2024-04-25 16:58:20 +0300

    Add update and delete methods.
    * Replace book repository using JpaRepository.
    
    * Change ddl-auto property value to validate.
    
    * Implement the soft delete concept in the book entity.
    
    * Add update and delete by id methods.
    
    * Add Liquibase support.
    
    * Remove nullable constraint from id column.

--- src/main/java/makaroshyna/onlinebookstore/controller/BookController.java
+++ src/main/java/makaroshyna/onlinebookstore/controller/BookController.java
@@ -5,11 +5,15 @@ import lombok.RequiredArgsConstructor;
 import makaroshyna.onlinebookstore.dto.BookDto;
 import makaroshyna.onlinebookstore.dto.CreateBookRequestDto;
 import makaroshyna.onlinebookstore.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -29,7 +33,19 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
-        return bookService.save(bookDto);
+    public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
+        return bookService.save(requestDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBookById(@PathVariable Long id,
+                                  @RequestBody CreateBookRequestDto requestDto) {
+        return bookService.updateById(id, requestDto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteBookById(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 }
--- src/main/java/makaroshyna/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/makaroshyna/onlinebookstore/service/impl/BookServiceImpl.java
@@ -36,4 +36,19 @@ public class BookServiceImpl implements BookService {
                 new EntityNotFoundException("Can't find book by id: " + id));
         return bookMapper.toDto(book);
     }
+
+    @Override
+    public BookDto updateById(Long id, CreateBookRequestDto requestDto) {
+        if (bookRepository.existsById(id)) {
+            Book book = bookMapper.toModel(requestDto);
+            book.setId(id);
+            return bookMapper.toDto(bookRepository.save(book));
+        }
+        throw new EntityNotFoundException("Can't find book by id: " + id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
