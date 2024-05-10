f6c03f4dc6a9edfac3901dabaa8e50bb54119b68
makaroshyna <katya23225@gmail.com>
2024-05-10 13:12:00 +0300

    Change fetch type lazy -> eager.

--- src/main/java/makaroshyna/onlinebookstore/model/User.java
+++ src/main/java/makaroshyna/onlinebookstore/model/User.java
@@ -42,7 +42,7 @@ public class User implements UserDetails {
 
     private String shippingAddress;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
