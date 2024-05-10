adb323cde25188feb5e1bebeb2a08e80193ebc6b
makaroshyna <katya23225@gmail.com>
2024-05-10 13:21:55 +0300

    Fix checkstyle.

--- src/main/java/makaroshyna/onlinebookstore/repository/user/UserRepository.java
+++ src/main/java/makaroshyna/onlinebookstore/repository/user/UserRepository.java
@@ -3,7 +3,6 @@ package makaroshyna.onlinebookstore.repository.user;
 import java.util.Optional;
 import makaroshyna.onlinebookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
