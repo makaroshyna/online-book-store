589f50bb70ffe24e49a9fc7589eabd75c62b8596
makaroshyna <katya23225@gmail.com>
2024-05-15 10:21:46 +0300

    Change data -> getter + setter.

--- src/main/java/makaroshyna/onlinebookstore/model/Category.java
+++ src/main/java/makaroshyna/onlinebookstore/model/Category.java
@@ -6,12 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
-@Data
+@Getter
+@Setter
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
 @SQLRestriction("is_deleted=false")
 @Table(name = "categories")
