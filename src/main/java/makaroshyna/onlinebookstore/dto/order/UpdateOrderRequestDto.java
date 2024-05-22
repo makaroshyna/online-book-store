7ef31bb9c035ba3e00fc831f57f6f3be936951b7
makaroshyna <katya23225@gmail.com>
2024-05-22 15:48:08 +0300

    Add not null annotation for status update dto.

--- src/main/java/makaroshyna/onlinebookstore/dto/order/UpdateOrderRequestDto.java
+++ src/main/java/makaroshyna/onlinebookstore/dto/order/UpdateOrderRequestDto.java
@@ -1,9 +1,11 @@
 package makaroshyna.onlinebookstore.dto.order;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import makaroshyna.onlinebookstore.model.Order.Status;
 
 @Data
 public class UpdateOrderRequestDto {
+    @NotNull
     private Status status;
 }
