2d03c2adb4f8d73a968fa3daa12ab6eedf397aec
makaroshyna <katya23225@gmail.com>
2024-06-07 17:22:06 +0300

    Resolve conflicts with branches.

--- src/test/resources/database/books/add-three-books.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO books (id, title, author, isbn, price) VALUES (1, 'The Fellowship of the Ring', 'J. R. R. Tolkien', '9780007136599', 520.55);
-INSERT INTO books (id, title, author, isbn, price) VALUES (2, 'The Two Towers', 'J. R. R. Tolkien', '9780007136568', 490.95);
-INSERT INTO books (id, title, author, isbn, price) VALUES (3, 'The Hound of the Baskervilles', 'Sir Arthur Conan Doyle', '0-14-020823-7', 179.95);
