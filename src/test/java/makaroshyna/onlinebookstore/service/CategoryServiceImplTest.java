5f9723ea0e593ff5c226c3a38badf19895f70bcd
makaroshyna <katya23225@gmail.com>
2024-06-11 17:09:35 +0300

    Fix checkstyle.

--- src/test/java/makaroshyna/onlinebookstore/service/CategoryServiceImplTest.java
+++ src/test/java/makaroshyna/onlinebookstore/service/CategoryServiceImplTest.java
@@ -3,7 +3,6 @@ package makaroshyna.onlinebookstore.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
