42065ef18cbf2626b86d28a026d210bcbbd80383
Katia Makarchuk <83081198+makaroshyna@users.noreply.github.com>
2025-03-07 22:51:57 +0200

    Add tests for Books and Categories.
    * Add tests.
    
    * Change bad request -> not found in bad scenario on update method.
    
    * Rename path to sql file.
    
    * Add empty line to file ends.
    
    * Remove repetitive code.
    
    * Remove repetitive code.
    
    * Remove times(1).
    
    * Fix checkstyle.
    
    * Resolve mentor comments.
    
    * Remove inject mocks.

---
+++ src/test/resources/database/categories/add-three-categories.sql
@@ -0,0 +1,3 @@
+INSERT INTO categories (id, name, description) VALUES (1, 'Fantasy', 'Fantasy Description');
+INSERT INTO categories (id, name, description) VALUES (2, 'Romance novel', 'Romance novel Description');
+INSERT INTO categories (id, name, description) VALUES (3, 'Detective', 'Detective Description');
